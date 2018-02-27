2520fdb345cd224c7bb280db9c60d8ecffb7f819
qdlee <plumllydev@gmail.com>
2018-02-27 14:16:30 +0800

    add header信息

--- app/api/index.js
+++ app/api/index.js
@@ -1,11 +1,14 @@
 const Koa = require('koa');
 const https = require('https');
+const http = require('http');
 const querystring = require('querystring');
 const fs = require('fs');
 
 const app = new Koa();
 
-const host = 'testapi.blkee.com';
+// const host = 'testapi.blkee.com';
+const host = 'localhost';
+const port = 5500;
 
 function request(ctx, method = 'POST') {
   return new Promise((resolve, reject) => {
@@ -19,18 +22,21 @@ function request(ctx, method = 'POST') {
     } else if (type.search('application/x-www-form-urlencoded') > -1) {
       postData = querystring.stringify(ctx.request.body);
     }
+    const myHeaders = {
+      'Content-Type': type,
+      'Content-Length': Buffer.byteLength(postData),
+      'User-Agent':
+        'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1 MicroMessenger/6.10.5',
+    };
+    const headers = { ...ctx.header, ...myHeaders };
     const options = {
       host,
       path: ctx.path,
+      port,
       method,
-      headers: {
-        'Content-Type': type,
-        'Content-Length': Buffer.byteLength(postData),
-        'User-Agent':
-          'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1 MicroMessenger/6.10.5',
-      },
+      headers,
     };
-    const req = https.request(options, res => {
+    const req = http.request(options, res => {
       resolve(res);
     });
     req.on('error', e => {
@@ -43,7 +49,12 @@ function request(ctx, method = 'POST') {
 
 app.use(async (ctx, next) => {
   const res = await request(ctx);
-  ctx.set('Content-Type', 'application/json;charset=utf-8');
+  const headers = res.headers;
+  Object.keys(headers).forEach(key => {
+    ctx.set(key, headers[key]);
+  });
+  ctx.status = res.statusCode;
+  ctx.message = res.statusMessage;
   ctx.body = res;
 });
 
